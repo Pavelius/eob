f4a0f0b2f0984605b4ede0dd4337cef22fd4ecdd
Pavel Chistyakov <p000000000001@gmail.com>
2019-06-11 21:26:04 +0300

    Рефактор

--- source/creature_chating.cpp
+++ source/creature_chating.cpp
@@ -1,12 +1,15 @@
 #include "main.h"
 
-static dialogi cautions_dialog[] = {{{}, "main", "\"Who is there? How you dig so deep? Are you from Master?\"", {{"Lie"}, {"Attack"}}},
+static dialogi cautions_dialog[] = {{{}, "main", "\"Who is there? How you dig so deep? Are you from Master?\"",
+{{"Lie", {}}, {"Attack", {}, {StartCombat}}}},
 {}};
 static dialogi friendly_dialog[] = {{{}, "main", "\"Welcome friends! Do you want something?\"", {{"Talk"}, {"Trade"}, {"Attack"}, {"Leave"}}},
 {}};
 
 void creature::encounter(reaction_s id) {
 	auto ins = get(Intellegence);
+	if(ins < 6)
+		return;
 	switch(id) {
 	case Cautious:
 		location.turnto(game::getcamera(), to(direction, Down));
