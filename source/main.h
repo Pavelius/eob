c25f6bc65c622f78c6cf353b5fc450bc1c94e135
Pavelius <p000000000001@gmail.com>
2023-02-20 18:20:44 +0200

    Remove variant depency

--- source/main.h
+++ source/main.h
@@ -1,7 +1,5 @@
 ﻿#include "alignment.h"
 #include "color.h"
-#include "crt.h"
-#include "cflags.h"
 #include "dataset.h"
 #include "dice.h"
 #include "duration.h"
@@ -17,6 +15,7 @@
 #include "size.h"
 #include "shape.h"
 #include "stringbuilder.h"
+#include "variant.h"
 #include "variable.h"
 
 const unsigned short Blocked = 0xFFFF;
@@ -214,15 +213,6 @@ enum talk_s : unsigned char {
 	Greeting,
 	FailLie, TalkAbout, TalkArtifact, TalkCursed, TalkMagic, TalkLoot, TalkLootCellar, TalkHistory, TalkRumor,
 };
-enum variant_s : unsigned char {
-	NoVariant,
-	Ability, Action, Adventure, Alignment, Case, Cell, Class,
-	Cleveress, Condition, Creature, Damage, Enchant, Feat, Gender,
-	Item, Morale, Race, Rarity, Reaction, Resource, Spell,
-};
-enum case_s : unsigned char {
-	Case1, Case2, Case3, Case4, Case5, Case6, Case7, Case8, Case9,
-};
 enum pack_s : unsigned char {
 	PackDungeon, PackMonster, PackOuttake,
 	PackInterface, PackBackground, PackCenter,
@@ -242,7 +232,6 @@ enum actionf_s : unsigned char {
 };
 typedef cflags<action_s> actionf;
 typedef cflags<good_s> goodf;
-typedef cflags<variant_s> variantf;
 typedef cflags<feat_s> feata;
 typedef cflags<usability_s> usabilitya;
 typedef flagable<1 + LastSpellAbility / 8> spellf;
@@ -254,44 +243,6 @@ class creature;
 class creaturea;
 class item;
 struct settlementi;
-struct variant {
-	variant_s		type;
-	unsigned char	value;
-	constexpr variant() : type(NoVariant), value(0) {}
-	constexpr variant(const variant_s t, unsigned char v) : type(t), value(v) {}
-	constexpr variant(const ability_s v) : type(Ability), value(v) {}
-	constexpr variant(const action_s v) : type(Action), value(v) {}
-	constexpr variant(const alignment_s v) : type(Alignment), value(v) {}
-	constexpr variant(const case_s v) : type(Case), value(v) {}
-	constexpr variant(const cell_s v) : type(Cell), value(v) {}
-	constexpr variant(const class_s v) : type(Class), value(v) {}
-	constexpr variant(const condition_s v) : type(Condition), value(v) {}
-	constexpr variant(const damage_s v) : type(Damage), value(v) {}
-	constexpr variant(const enchant_s v) : type(Enchant), value(v) {}
-	constexpr variant(const feat_s v) : type(Feat), value(v) {}
-	constexpr variant(const gender_s v) : type(Gender), value(v) {}
-	constexpr variant(const intellegence_s v) : type(Cleveress), value(v) {}
-	constexpr variant(const item_s v) : type(Item), value(v) {}
-	constexpr variant(const morale_s v) : type(Morale), value(v) {}
-	constexpr variant(const race_s v) : type(Race), value(v) {}
-	constexpr variant(const rarity_s v) : type(Rarity), value(v) {}
-	constexpr variant(const reaction_s v) : type(Reaction), value(v) {}
-	constexpr variant(const resource_s v) : type(Resource), value(v) {}
-	constexpr variant(const spell_s v) : type(Spell), value(v) {}
-	constexpr variant(const int v) : type(variant_s((v >> 8) & 0xFF)), value(v & 0xFF) {}
-	variant(variant_s v, const void* p);
-	variant(const void* v);
-	constexpr explicit operator bool() const { return type != NoVariant; }
-	constexpr explicit operator int() const { return (type << 8) | value; }
-	constexpr bool operator==(const variant& e) const { return type == e.type && value == e.value; }
-	void				clear() { type = NoVariant; value = 0; }
-	creature*			getcreature() const;
-	const char*			getname() const;
-	void*				getpointer(variant_s t) const;
-};
-typedef variant conditiona[6];
-struct varianta : adat<variant, 12> {
-};
 struct celli {
 	const char*			name;
 	resource_s			resource;
@@ -371,13 +322,6 @@ struct usabilityi {
 };
 struct itemfeati {
 	const char*			name;
-};
-struct varianti {
-	const char*			name;
-	const char*			namepl;
-	variantf			allowed;
-	array*				source;
-	static variant_s	find(const array* v);
 };
 struct combati {
 	char				number_attacks_p2r;
